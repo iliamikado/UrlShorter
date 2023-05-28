bb9a0f60d7b78a869fe8918644fa39e18cb3ddbf
iliamikado <iliamikado@gmail.com>
2023-05-28 15:48:41 +0300

    Launch serber

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+    "net/http"
+	"fmt"
+)
+
+func CheckRequest(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		MakeShortUrl(w, r)
+	case http.MethodPost:
+		MakeShortUrl(w, r)
+	default:
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "Wrong method")
+	}
+}
+
+func MakeShortUrl(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hi"))
+}
+
+func GetUrlById(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hi"))
+}
+
+
+func main() {
+    http.HandleFunc("/", CheckRequest)
+	
+    http.ListenAndServe(":8080", nil)
+} 
